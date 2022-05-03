14d7c0d3ae6b2c920e1c164d5619ccb1291a1ea6
joesphan <joesphanlu@gmail.com>
2022-05-03 05:40:44 -0700

    s

--- code/Blink_test/blink/main/blink.c
+++ code/Blink_test/blink/main/blink.c
@@ -15,7 +15,7 @@
 /* Can use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
    or you can edit the following line and set a number here.
 */
-#define led 27
+#define led 25
 
 void app_main(void)
 {
